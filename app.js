f9a95ae858ed927ae500626f2314596347fbb68a
hihell <jiu694@gmail.com>
2015-11-11 16:56:00 +0800

    test mongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var path = require('path')
 var app = express();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
 
 app.set('view engine', 'ejs');
 //app.use(express.static(__dirname + '../public'));
@@ -38,6 +40,8 @@ app.get('/uid/:uid/date/:datestr/show_evidence/:show_evidence/convert/:convert',
 
     var date = new Date(req.params.datestr)
     var startEnd = tsStartEnd(date)
+
+    //due to strongloop problem, need to read data from backend then send to front
 
     var data = {
         'data': {
@@ -83,7 +87,7 @@ app.get('/phone/:phone', function (req, res) {
 
 app.get('/basestation/start/:start/end/:end', function (req, res) {
     var data = {
-        'data' : {
+        'data': {
             'start': req.params.start,
             'end': req.params.end
         }
@@ -103,8 +107,8 @@ app.get('/basestation/date/:date', function (req, res) {
 
     var data = {
         'data': {
-            'start': startEnd[0]/1000,
-            'end': startEnd[1]/1000
+            'start': startEnd[0] / 1000,
+            'end': startEnd[1] / 1000
         }
     }
 
@@ -142,3 +146,111 @@ var tsStartEnd = function (date) {
 
     return [tsStart, tsEnd]
 }
+
+var getUPoiEvi = function (uid) {
+    var av = null
+    var mo = null
+    var promise = new AV.Promise()
+
+    getMoUPoiEvi(upois).then(function (data) {
+        mo = data
+        return getAvUPoiEvi(upois)
+    }).then(function (data) {
+        av = data
+        promise.resolve({'av': av, 'mo': mo})
+    })
+
+    return promise
+}
+
+var getMoUPoiEvi = function (upois) {
+    var promise = new AV.Promise()
+
+    var upoiids = []
+    for (var i = 0; i < upois.length; i++) {
+        upoiids.push(upois[i].id)
+    }
+
+    var where = '{"where": {"u_poi_id": {"inq": ' + JSON.stringify(upoiids) + '}}}'
+    var path = 'http://119.254.111.40:3000/api/MarkedUserLocation?filter=' + where
+
+    $.getJSON(path, function (data) {
+        if (data) {
+            promise.resolve(data)
+        } else {
+            promise.reject('load failed:', path)
+        }
+    })
+
+    return promise
+
+}
+
+var getAvUPoiEvi = function (upois) {
+
+    MUL.find({
+        'u_poi_id': {
+            $in: ['563af44160b25a751b806488', '563af445ddb2160c6a2b1e63']
+        }
+    }).then(function (data) {
+        console.log(data)
+    })
+}
+
+var testfind = function () {
+
+    where = {u_poi_id: '563af44160b25a751b806488'}
+
+    MUL.find(where, function (err, data) {
+        console.log('err:', err)
+        console.log('data:', data)
+    })
+}
+
+var testall = function() {
+
+    var q = MUL.find().limit(2)
+    q.exec(function (e, d) {
+        console.log('e:', e)
+        console.log('d:', d)
+    })
+
+    var q = UL.find().limit(2)
+    q.exec(function (e, d) {
+        console.log('e:', e)
+        console.log('d:', d)
+    })
+    
+}
+
+//Lets connect to our database using the DB server URL.
+mongoose.connect('mongodb://senzhub:Senz2everyone@119.254.111.40/RefinedLog')
+
+/**
+ * Lets define our Model for User entity. This model represents a collection in the database.
+ * We define the possible schema of User document and data types of each field.
+ * */
+//var User = mongoose.model('User', {name: String, roles: Array, age: Number})
+var MulSchema = {
+
+    u_poi_id: String
+    //"user_location": {
+    //    "ref": "UserLocation",
+    //    type: Schema.Types.ObjectId
+    //},
+    //"u_poi_visit_id": String,
+    //"location": {
+    //    "lat": Number,
+    //    "lng": Number
+    //},
+    //"timestamp": Number
+}
+
+var ULSchema = {
+    'user_id': String
+}
+
+var MUL = mongoose.model('MarkedUserLocation', MulSchema)
+var UL = mongoose.model('UserLocation', ULSchema)
+
+testall()
